7719bf91d3aea611ce4d7c79964593a0fb7e8f98
Tony Wang <zichutonywang@gmail.com>
2021-02-13 15:36:01 -0500

    Updated documentation.Populated configsetup.py

---
+++ configsetup.py
@@ -0,0 +1,7 @@
+# .gitignore should include reference to config.py
+# configsetup.py should be duplicated to config.py to take effect
+
+client_id = "personal use script"
+secret_token = "secret token"
+username = "username"
+password = "password"
